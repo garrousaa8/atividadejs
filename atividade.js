9e52d604abd67b2c36c382e5169c8308c12c3547
garrousaa8 <silvacalebe290@gmail.com>
2025-10-17 14:13:54 -0300

    first commit

--- atividade.js
+++ atividade.js
@@ -1,9 +1,9 @@
 let nomes = ['Maria', 'João', 'José'];
-nomes.forEach((nomes) => console.log(nomes))
+nomes.forEach((nomes) => console.log(nomes));
 
 let numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-const numerosdobrados = numeros.map((numeros) => (numeros * 2));
-console.log(numerosdobrados)
+const numerosDobrados = numeros.map((numeros) => (numeros * 2));
+console.log(numerosDobrados);
 
 const numerosImpares = numeros.filter(numeros => numeros % 2 !== 0);
-console.log(numerosImpares)
+console.log(numerosImpares);
